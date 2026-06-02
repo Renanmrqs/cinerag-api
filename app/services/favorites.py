4ce6ba18d02aaa4f3e51c863714fdaf7df9d79b7
Renanmrqs <renanmarques1923@gmail.com>
2026-06-02 13:01:25 -0300

    nova rota favoritos

--- app/services/favorites.py
+++ app/services/favorites.py
@@ -4,8 +4,9 @@ from sqlalchemy import select
 from datetime import date
 
 def read_all_favorites(user_id, db:Session) -> dict:
-    favorite_table = db.query(Favorites.id, Favorites.added_at, Movies.title).filter(Favorites.user_id == user_id).join(Movies, Favorites.movie_id == Movies.id).all()
-    return [{"id": r.id, "title": r.title, "added_at": r.added_at} for r in favorite_table]
+    favorite_table = db.query(Favorites.id, Favorites.added_at, Movies.title, Movies.sentiment, Movies.sentiment_trust).filter(Favorites.user_id == user_id).join(Movies, Favorites.movie_id == Movies.id).all()
+    return [{"id": r.id, "title": r.title, "added_at": r.added_at, "sentiment": r.sentiment, "trust": r.sentiment_trust} for r in favorite_table]
+    
 
 def create_favorite(user_id, movie_id, db:Session) -> dict:
     favorite = Favorites(user_id=user_id, movie_id=movie_id, added_at=date.today())
