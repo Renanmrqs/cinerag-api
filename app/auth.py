77f9c1404447e7e66f8c8a1467cf1f6f03c8c7b6
Renanmrqs <renanmarques1923@gmail.com>
2026-06-10 19:57:55 -0300

    fix: ajustando o verify token

--- app/auth.py
+++ app/auth.py
@@ -20,7 +20,7 @@ def verify_token(token: str):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
     
-        username = payload.get("sub")
+        username = payload.get("username")
         if username is None: 
             raise HTTPException(status_code=401, detail="Invalid token")
         return username
