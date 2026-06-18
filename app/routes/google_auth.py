df32cf9f1644931352120043f2273b784954897a
Renanmrqs <renanmarques1923@gmail.com>
2026-06-18 18:44:51 -0300

    fetch: refatorando a rota do auth com o google

--- app/routes/google_auth.py
+++ app/routes/google_auth.py
@@ -2,12 +2,10 @@ from authlib.integrations.starlette_client import OAuth
 import os
 from fastapi import Depends, Request, APIRouter
 from fastapi.responses import RedirectResponse
-from datetime import timedelta
 from dotenv import load_dotenv
 from app.database import get_db
 from sqlalchemy.orm import Session
-from app.auth import generate_token
-from app.services.users_service import get_users_by_identifier, create_register_google
+from app.services.auth_service import google_auth_service
 
 load_dotenv()
 
@@ -28,26 +26,12 @@ oauth.register(
 async def auth_google(request: Request):
     return await oauth.google.authorize_redirect(request, redirect_uri="https://cinerag-api.onrender.com/auth/google/callback")
 
+
 @router.get("/auth/google/callback", tags=["auth"])
 async def google_callback(request: Request, db: Session = Depends(get_db)):
     try:
         token = await oauth.google.authorize_access_token(request)
-        user_info = token.get("userinfo") or {}
-
-        # Extract user details
-        email = user_info.get("email")  # Use email as username
-
-        # Generate a JWT token with auth_method="google"
-        user = get_users_by_identifier(email, db)
-        
-        if not user:
-            user = create_register_google(db, email, email, "google_oauth")
-            access_token = generate_token(email, user.id)
-            frontend_url = f'https://cinerag-analytics.vercel.app/google_register.html?token={access_token}'
-            return RedirectResponse(url=frontend_url)
-        access_token = generate_token(email, user.id)
-        
-        frontend_url = f'https://cinerag-analytics.vercel.app/index.html?token={access_token}'
+        frontend_url = google_auth_service(token, db)
         return RedirectResponse(url=frontend_url)
     except Exception as e:
         import traceback
--- app/ws/test.py
+++ app/ws/test.py
@@ -2,9 +2,9 @@ import asyncio
 import websockets
 
 async def test():
-    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMjQiLCJ1c2VybmFtZSI6InJlbmFuIiwiZXhwIjoxNzgxNzU2ODA0fQ.kmIKkOCVpAdM61sWLtKxTnLz0Z5iguOFOE6EMN7Q_m4"
+    token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMjQiLCJ1c2VybmFtZSI6InJlbmFuIiwiZXhwIjoxNzgxODI0OTE2fQ.jhDR6pFufuGKkxiOyBKgPzXMUkQJdZ61HQ1wfDS6kfg"
     async with websockets.connect(f"ws://127.0.0.1:8000/ws?token={token}") as ws:
-        await ws.send("oi recomandame um fiklme")
+        await ws.send("fala um filme ai")
         print(await ws.recv())
         
 
